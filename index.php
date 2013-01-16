44b0a945de612e8546a4426f3193787e4f4e47a9
Kolesias123 <webmaster@infosmart.mx>
2013-01-16 17:04:05 -0600

    InfoSmart Cuentas v1

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <?
 require 'Init.php';
 
+## --------------------------------------------------
+## Home
+## --------------------------------------------------
+## Esta página es solicitada por AJAX cuando el usuario
+## cambia su información.
+## --------------------------------------------------
+
+# No hemos iniciado sesión.
 if(!LOG_IN)
 	Core::Redirect('/connect/login');
 
+# Obtener una lista de meses en el idioma del usuario.
 $months 	= Date::GetListMonths();
+# Obtener una lista de los países.
 $countrys 	= Site::Get(); 
+# Separar la fecha de nacimiento en día, mes y año.
 $birth 		= explode('/', $me['birthday']);
 
+# Plantilla: home.html
+# Plantilla secundaria: home.bar.html (Barra de la derecha)
 $page['id'] 	= array('home', 'home.bar');
+# Con esto tenemos los estilos y scrips apropiados.
 $page['home']	= true;
 ?>
--- photo.php
+++ photo.php
@@ -1,24 +1,31 @@
 <?
 require 'Init.php';
 
+# El usuario solicitado no existe o queremos la foto "default"
 if(!Users::UserExist($G['user']) OR $G['user'] == 'default')
 {
 	$data = AcUsers::GetPhoto('default');
 	goto ShowPhoto;
 }
 
+# ¿De que tamaño quieres la imagen?
 $size = $G['size'];
 
-if(empty($size))
+# Por predeterminado de tamaño medio.
+if( empty($size) )
 	$size = 'medium';
 
+# Obtenemos los "bits" de la foto del usuario.
 $data = AcUsers::GetPhoto($G['user'], $size);
 
 ShowPhoto:
 {
+	# Permitir cargar desde cualquier dominio.
 	Tpl::AllowCross('*');
+	# Mostrar como imagen PNG
 	Tpl::Image();
 
+	# Lanzar "bits"
 	echo $data;
 }
 ?>

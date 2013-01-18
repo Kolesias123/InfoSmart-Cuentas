82dcde76f557a24ef341ef49ca55c0ea82e12fb5
Kolesias123 <webmaster@infosmart.mx>
2013-01-18 15:59:37 -0600

    InfoSmart Cuentas v1 - API para los productos de InfoSmart. - Se agregaron comentarios.

--- api/profile.bar.php
+++ api/profile.bar.php
@@ -1,14 +1,26 @@
 <?
 require '../Init.php';
 
+## --------------------------------------------------
+## Barra de perfil
+## --------------------------------------------------
+## La barra de perfil es una barra que se mostrará
+## en todas las aplicaciones y productos web de InfoSmart.
+## Su objetivo es el de notificar el usuario que ha iniciado
+## sesión en el computador y además ofrecer la navegación
+## de la aplicación.
+## --------------------------------------------------
+
+# Permitir carga desde cualquier dominio.
 Tpl::AllowCross('*');
 
+# Plantilla: profile.bar.html (Si ha iniciado sesión) o profile.bar.default.html (Si no)
 $page['id'] 		= (LOG_IN) ? 'profile.bar' : 'profile.bar.default';
 
-
+# Carpeta: /api/
 $page['folder'] 	= 'api';
+# Sin cabecera
 $page['header'] 	= false;
+# Sin pie de página
 $page['footer']		= false;
-$page['subheader']	= false;
-$page['subfooter']  = false;
 ?>
